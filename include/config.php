9e6056fa417ca3f3bec21ebb31f37edac7819b5f
sanjaykumara1994 <sanjaykumara1994@gmail.com>
2015-03-21 10:35:32 -0700

    Initial Commit

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "sanjay");
+define("DB_PASSWORD", "seanunbound123");
+define("DB_DATABASE", "main");
+?>
